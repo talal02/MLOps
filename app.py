6b59bc94fe6740130dda7b2785fa1d6ae20c94ac
Talal Ahmed <talalahmed252@gmail.com>
2023-03-14 00:03:41 +0500

    Updates

--- app.py
+++ app.py
@@ -5,29 +5,9 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+regressor = None
+
 app = Flask(__name__)
-
-# Load dataset from CSV file
-df = pd.read_csv('Salary_Data.csv')
-
-# Split data into input and output variables
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
-
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-# Train linear regression model
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-
-# Predict salaries on test set
-y_pred = regressor.predict(X_test)
-
-# Print model coefficients and accuracy
-print("Coefficients:", regressor.coef_)
-print("Intercept:", regressor.intercept_)
-print("Accuracy:", regressor.score(X_test, y_test))
 
 @app.route('/')
 def home():
@@ -41,4 +21,25 @@ def predict(years_of_experience):
     return str(predicted_salary[0])
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    # Load dataset from CSV file
+    df = pd.read_csv('Salary_Data.csv')
+
+    # Split data into input and output variables
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Train linear regression model
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    # Predict salaries on test set
+    y_pred = regressor.predict(X_test)
+
+    # Print model coefficients and accuracy
+    print("Coefficients:", regressor.coef_)
+    print("Intercept:", regressor.intercept_)
+    print("Accuracy:", regressor.score(X_test, y_test))
+    app.run(debug=True, host='0.0.0.0')
