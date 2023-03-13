5c742645d599326e12dc6cc25427743e45a8d6d6
Talal Ahmed <talalahmed252@gmail.com>
2023-03-13 23:29:59 +0500

    Updates

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
   stages {
     stage('Run') {
       steps {
-        bat 'docker run --name flask_app -p 8000:5000 temp'
+        bat 'docker run --name flask_app -p 8000:5000 mlops_flask'
       }
     }
   }
--- app.py
+++ app.py
@@ -1,44 +1,44 @@
 # Import necessary libraries
 from flask import Flask
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+# import pandas as pd
+# import numpy as np
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
-# Load dataset from CSV file
-df = pd.read_csv('Salary_Data.csv')
+# # Load dataset from CSV file
+# df = pd.read_csv('Salary_Data.csv')
 
-# Split data into input and output variables
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+# # Split data into input and output variables
+# X = df.iloc[:, :-1].values
+# y = df.iloc[:, -1].values
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# # Split data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Train linear regression model
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+# # Train linear regression model
+# regressor = LinearRegression()
+# regressor.fit(X_train, y_train)
 
-# Predict salaries on test set
-y_pred = regressor.predict(X_test)
+# # Predict salaries on test set
+# y_pred = regressor.predict(X_test)
 
-# Print model coefficients and accuracy
-print("Coefficients:", regressor.coef_)
-print("Intercept:", regressor.intercept_)
-print("Accuracy:", regressor.score(X_test, y_test))
+# # Print model coefficients and accuracy
+# print("Coefficients:", regressor.coef_)
+# print("Intercept:", regressor.intercept_)
+# print("Accuracy:", regressor.score(X_test, y_test))
 
 @app.route('/')
 def home():
     return "WELCOME TO MY APP"
 
-@app.route('/predict/<years_of_experience>')
-def predict(years_of_experience):
-    years_of_experience = float(years_of_experience)
-    predicted_salary = regressor.predict([[years_of_experience]])
-    print("Predicted salary for %0.1f years of experience: $%0.2f" % (years_of_experience, predicted_salary[0]))
-    return str(predicted_salary[0])
+# @app.route('/predict/<years_of_experience>')
+# def predict(years_of_experience):
+#     years_of_experience = float(years_of_experience)
+#     predicted_salary = regressor.predict([[years_of_experience]])
+#     print("Predicted salary for %0.1f years of experience: $%0.2f" % (years_of_experience, predicted_salary[0]))
+#     return str(predicted_salary[0])
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0')
